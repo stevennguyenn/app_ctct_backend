31b51a2a7320662993e0162f16544979efded559
Steve <chaunguyen4297@gmail.com>
2019-10-08 14:25:19 +0700

    add doctrine

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('scheme/create_user', function(){
+	Schema::create('user', function($table) {
+		$table -> increments('id');
+		$table -> string('name');
+		$table -> integer('age');
+		$table -> text('note') -> nullable();
+		$table -> string('password');
+		$table -> timestamps();
+		$table -> string('img_src') -> nullable();
+		$table -> string('token') -> nullable();
+	});
+});
+
+Route::get('scheme/rename', function() {
+	Schema::rename('User','user');
+});
+
+Route::get('scheme/change_attribute', function(){
+	Schema::table('User', function($table){
+		$table -> string('name', 50) -> change();
+	});
+});
+
+Route::get('scheme/deleteUser', function(){
+	Schema::dropIfExists("User");	
+});
